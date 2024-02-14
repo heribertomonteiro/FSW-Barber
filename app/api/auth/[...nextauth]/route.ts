a73407a43995b39acd9919033511b9249d56a972
heribertomonteiro <heribertoomonteiroo@gmail.com>
2024-02-14 12:04:13 -0300

    feat: ensure unauthenticated user is redirected to login on booking click

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,19 @@
-import NextAuth from "next-auth"
+import NextAuth, { AuthOptions } from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import GoogleProvider from "next-auth/providers/google"
 import { db } from "@/app/_lib/prisma"
 import { Adapter } from "next-auth/adapters"
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(db) as Adapter,
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     })
   ]
-})
+}
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -3,6 +3,8 @@ import { db } from "@/app/_lib/prisma";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import BarbershopInfo from "../_components/barbershop-info";
 import ServiceItem from "../_components/service-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface BarbershopDetailsPageProps {
   params: {
@@ -10,9 +12,8 @@ interface BarbershopDetailsPageProps {
   };
 }
 
-const BarbershopDetailsPage = async ({
-  params,
-}: BarbershopDetailsPageProps) => {
+const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
+  const session = await getServerSession(authOptions);
   if (!params.id) {
     //TODO: redirecionar home page
     return null;
@@ -36,7 +37,7 @@ const BarbershopDetailsPage = async ({
       <BarbershopInfo barbershop={barbershop} />
       <div className="px-5 flex flex-col gap-4 py-6">
         {barbershop.Services.map((service) => (
-          <ServiceItem key={service.id} service={service} />
+          <ServiceItem key={service.id} service={service} isAthenticated={!!session?.user}/>
         ))}
       </div>
     </div>
